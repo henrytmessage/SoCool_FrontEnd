97e1e6c2b7c900010259f7cc07d0571a00e0fcd5
duc dz <duc.tran@tmessage.net>
2024-07-03 15:20:37 +0700

    fix bug conversation id

--- src/components/ContentChat.tsx
+++ src/components/ContentChat.tsx
@@ -218,7 +218,7 @@ const ContentChat = () => {
 
           if (data.status_code === 200) {
             setInitConversation(data.data)
-            setConversationId(data.data.link.id)
+            setConversationId(data.data.id)
             setInitCurrency(data.data.link.currency)
             // setActionMessage(data.data.link.action)
             if (data.data.link.currency === 'USD') {
