340866d4abff1918280af44961f62c0660259bbe
duc dz <duc.tran@tmessage.net>
2024-07-09 11:24:07 +0700

    fetch ip location

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,47 +1,57 @@
 // src/i18n.ts
 
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
 
-import ENTranslate from "./language/EN-translate.json";
-import VNTranslate from "./language/VN-translate.json";
-import { checkLocationInVietnam } from './function';
+import ENTranslate from './language/EN-translate.json'
+import VNTranslate from './language/VN-translate.json'
+import { checkLocationInVietnam } from './function'
 
 // Translations
 i18n.use(initReactI18next).init({
   resources: {
     en: { translation: ENTranslate },
-    vn: { translation: VNTranslate },
+    vn: { translation: VNTranslate }
   },
-  lng: "en", // Ngôn ngữ mặc định
-  fallbackLng: "en", // Ngôn ngữ dự phòng
-  interpolation: { escapeValue: false },
-});
+  lng: 'en', // Ngôn ngữ mặc định
+  fallbackLng: 'en', // Ngôn ngữ dự phòng
+  interpolation: { escapeValue: false }
+})
 
-const storedLanguage = localStorage.getItem('language');
+const storedLanguage = localStorage.getItem('language')
 
 if (storedLanguage) {
-  i18n.changeLanguage(storedLanguage);
+  i18n.changeLanguage(storedLanguage)
 } else if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    (position) => {
-      const lat = position.coords.latitude;
-      const lng = position.coords.longitude;
-      if (checkLocationInVietnam(lat, lng)) {
-        i18n.changeLanguage('vn'); 
+  const fetchIp = async () => {
+    try {
+      const response = await fetch('https://api.ipify.org?format=json')
+      const data = await response.json()
+      fetchLocation(data.ip)
+    } catch (error) {
+      console.error('Error fetching IP:', error)
+    }
+  }
+
+  const fetchLocation = async (ip: string) => {
+    try {
+      const response = await fetch(`http://ip-api.com/json/${ip}`)
+      const data = await response.json()
+      if (data.countryCode === 'VN') {
+        i18n.changeLanguage('vn')
         localStorage.setItem('language', 'vn')
       } else {
-        i18n.changeLanguage('en');
+        i18n.changeLanguage('en')
         localStorage.setItem('language', 'en')
       }
-    },
-    (err) => {
-      console.log("err", err);
+    } catch (error) {
+      console.error('Error fetching location:', error)
     }
-  );
+  }
+
+  fetchIp()
 } else {
-  console.log("Geolocation is not supported by this browser.");
+  console.log('Geolocation is not supported by this browser.')
 }
 
-
-export default i18n;
+export default i18n
