4fcaee1c85b7ab0910176d620435dce27613f978
duc dz <duc.tran@tmessage.net>
2024-07-15 10:22:10 +0700

    fix axios instance

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,19 +1,32 @@
 import axios, { AxiosInstance, AxiosResponse, AxiosError } from 'axios';
 
+let currentLanguage = localStorage.getItem('language');
+
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_URL, // Replace with your API base URL
   timeout: 20000, // Example timeout configuration
   headers: {
     'Content-Type': 'application/json',
-    'accept-language': localStorage.getItem('language')
+    'accept-language': currentLanguage
   },
 });
+
+// Function to update axiosInstance headers
+const updateHeaders = () => {
+  const newLanguage = localStorage.getItem('language');
+  if (newLanguage !== currentLanguage) {
+    currentLanguage = newLanguage;
+    axiosInstance.defaults.headers['accept-language'] = newLanguage;
+  }
+};
+
+// Initial call to set headers
+updateHeaders();
 
 // Request interceptor for adding authorization token
 axiosInstance.interceptors.request.use(
   (config: any) => {
     // Perform actions before request is sent
-
     const accessToken = sessionStorage.getItem('access_token');
     const token = accessToken && JSON.parse(accessToken).token;
     if (token) {
@@ -46,5 +59,8 @@ axiosInstance.interceptors.response.use(
     return Promise.reject(error);
   }
 );
+
+// Listen to changes in localStorage 'language' key
+setInterval(updateHeaders, 1000); // Check every second for changes in 'language'
 
 export default axiosInstance;
