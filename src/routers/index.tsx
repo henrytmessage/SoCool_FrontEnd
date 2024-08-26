22438fb1327fb5647bba1014186f10ea7732da73
Hoàng Nguyễn <henry@tmessage.net>
2024-08-26 14:07:30 +0700

    fix bug

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -5,12 +5,13 @@ import ContentChat from '../components/ContentChat'
 import OnBoarding from '../components/OnBoarding'
 import Login from '../components/Login'
 import NotFound from '../components/NotFound'
+import UpComing from '../components/UpComing'
 
 const AppRouters = () => {
   return (
     <Routes>
       <Route path="/test" element={<Test />} />
-      <Route path="/" element={<OnBoarding />} />
+      <Route path="/" element={<UpComing />} />
       <Route path="/chat" element={<ContentChat />} />
       <Route path="/go" element={<Login />} />
       <Route path="*" element={<NotFound />} />
