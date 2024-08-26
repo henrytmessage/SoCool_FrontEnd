07257d71f28309199a3e6ab413a4fee0598f10df
Hoàng Nguyễn <henry@tmessage.net>
2024-08-26 14:07:54 +0700

    add

---
+++ src/components/UpComing.tsx
@@ -0,0 +1,10 @@
+const UpComing = () => {
+  return (
+    <div style={{ textAlign: 'center', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <p style={{ fontSize: '24px' }}>
+        We are working on repairs for the next version. Thank you for your interest!
+      </p>
+    </div>
+  )
+} 
+export default UpComing
