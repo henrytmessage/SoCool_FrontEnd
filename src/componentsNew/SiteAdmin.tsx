6bc11e87e5b1c522f18516a15c92f5c03ffce068
Hoàng Nguyễn <henry@tmessage.net>
2024-09-20 22:19:05 +0700

    fix

---
+++ src/componentsNew/SiteAdmin.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+
+const DashBoardPage = () => {
+  
+
+  const siteAdmin = {
+    
+      content: (
+      <div>
+        
+      </div>)
+    
+  }
+
+  useEffect(() => {
+  }
+  ,[])
+
+  return (<div>
+    <div className="ml-10 mt-10">
+    <strong>Admin</strong>
+    </div>
+    
+    {
+     <div/>
+    }
+
+  </div>);
+
+}
+
+export default DashBoardPage;
